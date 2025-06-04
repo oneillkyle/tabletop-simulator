37334fc3ac0b128952f8fa809bc3a791e76618d4
Kyle O'Neill <oneillkyle@gmail.com>
2025-06-03 21:54:14 -0700

    reload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,31 +3,37 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt'],
-      manifest: {
-        name: 'Tactical Skirmish',
-        short_name: 'Skirmish',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#000000',
-        theme_color: '#0ff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+    server: {
+        hmr: true,
+        watch: {
+            usePolling: true
+        }
+    },
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'robots.txt'],
+            manifest: {
+                name: 'Tactical Skirmish',
+                short_name: 'Skirmish',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#000000',
+                theme_color: '#0ff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 });
